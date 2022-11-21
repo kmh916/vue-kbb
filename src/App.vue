0c48ef0f4a823495ee8377c3c5e155f5c8f87fc0
김민혁 <94831670+kmh916@users.noreply.github.com>
2022-11-21 21:14:29 +0900

    vuetify sample Test

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <v-main>
+      <Nav></Nav>
+      <router-view/>
+    </v-main>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+import Nav from "@/views/Nav";
+export default {
+  name: 'App',
+  components: {Nav},
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-tabs
+      fixed-tabs
+      background-color="indigo"
+  >
+    <v-tab v-for="i in item" :key="item">
+      {{i}}
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script>
+export default {
+  name: "Nav.vue",
+  data: () => ({
+    item: ['k','b','c','d'],
+  }) ,
+
+}
+</script>
+
+<style scoped>
+
+</style>
